4f90b4ff3ea8e862af41e80e71f21adaa9e836b5
Afeez <azeezafeez119@gmail.com>
2023-03-15 11:28:49 +0100

    Learning to use git correctly

---
+++ index.js
@@ -0,0 +1,12 @@
+// git --version
+// git help
+// git --update
+// git config --global user.name "Afeez"
+// git config --global user.email "azeezafeez119@gmail.com"
+// git config --global color.ui auto
+// git config
+// git config -l
+// git add finename
+// git add .A (add files in current and parent dir)
+// git add . (add files in current dirctory) 
+
